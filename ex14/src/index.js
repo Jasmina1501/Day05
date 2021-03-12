b6859d930a7d6eaaabae0335c32c744cf9bb5391
Jasmina Secic <jasmina.secic98@outlook.com>
2021-03-12 18:09:09 +0100

    Word Blanks

---
+++ ex14/src/index.js
@@ -0,0 +1,11 @@
+function main() {
+    var myNoun = "dog ";
+    var myAdjective = "ran ";
+    var myVerb = "big ";
+    var myAdverb = "quickly.";
+
+    var wordBlanks = myNoun + myVerb + myAdjective + myAdverb;
+    return wordBlanks; 
+}
+console.log(main());
+module.exports = main;
