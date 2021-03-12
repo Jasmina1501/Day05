5269593327359c631fef09f22f64fa8de181cbe2
Jasmina Secic <jasmina.secic98@outlook.com>
2021-03-12 17:25:27 +0100

    Find the Length of a String

---
+++ ex08/src/index.js
@@ -0,0 +1,9 @@
+function main() {
+    var lastName = "Doe";
+    var lastNameLength;
+
+    lastNameLength = lastName.length;
+    return lastNameLength;
+}
+console.log(main());
+module.exports = main;
